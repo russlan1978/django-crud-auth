6aa6651e3f3ec8336c3b6daa6daa66489e1b6a78
Russlan <russlan1978@gmail.com>
2024-05-26 23:46:36 -0400

    prbando git 2

--- tasks/views.py
+++ tasks/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
-       return render(request, 'home.html1')
+       return render(request, 'home.html')
    
 def signup(request):
        if request.method == 'GET':
